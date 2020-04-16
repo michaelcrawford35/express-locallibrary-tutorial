7f2f0c7ee49a1009f1654da194ef9b710904d118
Michael Crawford <mcrawbeast35@gmail.com>
2020-04-16 17:08:03 -0500

    Added routing and secrets

--- app.js
+++ app.js
@@ -1,17 +1,21 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const dotenv = require('dotenv')
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+const catalogRouter = require('./routes/catalog');  //Import routes for "catalog" area of site
 
-var app = express();
+const app = express();
+
+dotenv.config({ path: '.env' })
 
 const mongoose = require('mongoose')
 
-const db_conn_str = 'mongodb+srv://admin:D45Sh30T70TppTRp@cluster0-ioqdg.mongodb.net/local_library?retryWrites=true&w=majority'
+const db_conn_str = process.env.MONGO_CONN
 
 mongoose.connect(db_conn_str, {useNewUrlParser: true, useUnifiedTopology: true})
 mongoose.Promise = global.Promise;
@@ -30,6 +34,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/catalog', catalogRouter);  // Add catalog routes to middleware chain.
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
